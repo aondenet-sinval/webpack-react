1e7de36a44aed9b688ca4e6bd21d19933c115683
aondenet-sinval <sinvalsatv@gmail.com>
2023-04-08 21:25:45 -0300

    commit 1

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+  const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+  module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    plugins: [
+      new HtmlWebpackPlugin({
+      title: 'Caching',
+      }),
+    ],
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+    optimization: {
+      moduleIds: 'deterministic',
+      runtimeChunk: 'single',
+     splitChunks: {
+       cacheGroups: {
+         vendor: {
+           test: /[\\/]node_modules[\\/]/,
+           name: 'vendors',
+           chunks: 'all',
+         },
+       },
+     },
+    },
+  };
